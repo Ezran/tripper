9d45d9ec55689685d4430f2e247f8fde01e7b4bb
Ezran <phantom.ezran@gmail.com>
2016-09-08 00:28:59 -0600

    basic db update interface added

--- imports/ui/segment.js
+++ imports/ui/segment.js
@@ -9,13 +9,41 @@ Template.trip.events({
         Markers.remove(this._id);
     },
 
-    'click .edit' () {
-        var tmp = document.getElementById("edit-point");
+    'click .edit' (event) {
+        var data = Markers.find({_id: this._id}).fetch();
+        var parent = $(event.target).parent();
+        var form = parent.next().children("form");
+        //fill form with known data
+        form.find("[name=title]").val(data[0].title);
+        form.find("[name=date]").val(data[0].date);
+        form.find("[name=start_time]").val(data[0].start_time);
+        form.find("[name=end_time]").val(data[0].end_time);
+        //replace all text with input box and save button html
+       parent.css("display", "none");
+       parent.next().css("display", "block"); 
+       
+    },
+
+    'click .save' (event) {
+        //update db with input boxes
+        var data = $(event.target).prev().serializeArray().reduce((obj, item) => {
+            obj[item.name] = item.value;
+            return obj;
+        }, {}); 
         Markers.update({_id: this._id}, {$set: {
-            title: tmp.elements[0].value,
-            date: tmp.elements[1].value,
-            start_time: tmp.elements[2].value,
-            end_time: tmp.elements[3].value
+            title: data.title,
+            date: data.date,
+            start_time: data.start_time,
+            end_time: data.end_time
         }});
+        //replace html input with text and edit button
+        $(event.target).parent().css("display", "none");
+        $(event.target).parent().prev().css("display", "block");
+    },
+
+    'click .cancel' (event) {
+        //replace html input with text and edit buttons
+        $(event.target).parent().css("display", "none");
+        $(event.target).parent().prev().css("display", "block");
     }
 });
